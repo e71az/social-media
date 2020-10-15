f6f71a487dbf9512f1eb4a0c5a8efb4764dfa085
s0kil <dtechtours@gmail.com>
2020-10-15 12:19:00 -0400

    Likes View Spec

---
+++ spec/views/posts/likes/index.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "posts/likes/index", type: :system do
+  it 'Likes Comment' do
+    login_user
+
+    visit posts_path
+    fill_in 'Content', with: 'Test Post'
+    click_button 'Save'
+
+    fill_in 'Add new Comment', with: 'Test Comment'
+    click_button 'Comment'
+
+    click_link 'Like!'
+
+    expect(page).to have_content('1 likes')
+  end
+end
