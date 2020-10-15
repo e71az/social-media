d5e311beee1958746d0b0fbc773872736893d436
s0kil <dtechtours@gmail.com>
2020-10-15 13:35:02 -0400

    Mutual Friendship Spec

--- spec/views/friendships/index.html.erb_spec.rb
+++ spec/views/friendships/index.html.erb_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-  def create_users
-    user1 = login_user
-    user2 = create(:user, name: "Daniel")
-    return user1, user2
-  end
+def create_users
+  user1 = login_user
+  user2 = create(:user, name: "Daniel")
+
+  [user1, user2]
+end
 
 RSpec.describe "friendships/index", type: :system do
-
   describe "Friendship manipulation" do
     it "Send a friendship request" do
       create_users
@@ -56,6 +56,31 @@ RSpec.describe "friendships/index", type: :system do
       visit users_path
 
       expect(page).to have_button('Send Friend Request')
+    end
+
+    it "Mutual Friendship" do
+      user1, user2 = create_users
+
+      # Send Friend Request To User2
+      visit users_path
+      click_button 'Send Friend Request'
+      click_link 'Sign out'
+
+      # Accept Friend Request From User1
+      login_user(user2)
+      visit friendships_path
+      click_button 'Accept'
+
+      # Send Friend Request To User1
+      visit users_path
+      click_button 'Send Friend Request'
+      click_link 'Sign out'
+
+      # Accept Friend Request From User2
+      login_user(user1)
+      visit users_path
+
+      expect(page).to have_button('Unfriend')
     end
   end
 end
