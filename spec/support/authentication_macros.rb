9951a0b8bf7ffd312343dce928d9dfa236c96dcc
s0kil <dtechtours@gmail.com>
2020-10-15 11:55:38 -0400

    Stop Sleeping, Wake up, Get To Work.

--- spec/support/authentication_macros.rb
+++ spec/support/authentication_macros.rb
@@ -9,12 +9,5 @@ module AuthenticationMacros
     fill_in('user[email]', with: user.email)
     fill_in('user[password]', with: user.password)
     click_button('Log in')
-    sleep(1)
-
-    # Register User
-    # visit new_user_registration_path
-    # fill_form(:user, user_attributes)
-    # click_button('Sign up')
-    # sleep(1)
   end
 end
