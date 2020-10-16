95282eca321484d8091b8e98506631f031b2350c
Elias Castañeda <e71az@icloud.com>
2020-10-16 11:11:27 -0600

    Test creation for Irreflexive friendships

--- spec/friendships/index.html.erb_spec.rb
+++ spec/friendships/index.html.erb_spec.rb
@@ -1,24 +1,18 @@
 require 'rails_helper'
 
-def create_users
-  user1 = login_user
-  user2 = create(:user, name: 'Daniel')
-
-  [user1, user2]
-end
-
 RSpec.describe 'friendships/index', type: :system do
   describe 'Friendship manipulation' do
     it 'Send a friendship request' do
-      create_users
-
+      user1, _user2 = create_users
+      login_user(user1)
       visit users_path
       click_button 'Send Friend Request'
       expect(page).to have_button('Cancel Request')
     end
 
     it 'Cancel a friend request' do
-      create_users
+      user1, _user2 = create_users
+      login_user(user1)
 
       visit users_path
       click_button 'Send Friend Request'
@@ -28,6 +22,7 @@ RSpec.describe 'friendships/index', type: :system do
 
     it 'Accept a friend request' do
       user1, user2 = create_users
+      login_user(user1)
 
       visit users_path
       click_button 'Send Friend Request'
@@ -44,6 +39,7 @@ RSpec.describe 'friendships/index', type: :system do
 
     it 'Reject a friend request' do
       user1, user2 = create_users
+      login_user(user1)
 
       visit users_path
       click_button 'Send Friend Request'
@@ -60,6 +56,7 @@ RSpec.describe 'friendships/index', type: :system do
 
     it 'Mutual Friendship' do
       user1, user2 = create_users
+      login_user(user1)
 
       # Send Friend Request To User2
       visit users_path
---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Friendship, type: :model do
+  describe "Friendship model testing" do
+    it "Irreflexive Friendship test" do
+      user1, _user2 = create_users
+      friendship = Friendship.new(user: user1, friend: user1)
+
+      expect(friendship).to_not be_valid
+    end
+    it "Succesful friendship" do
+      user1, user2 = create_users
+      friendship = Friendship.new(user: user1, friend: user2)
+
+      expect(friendship).to be_valid
+    end
+  end
+end
