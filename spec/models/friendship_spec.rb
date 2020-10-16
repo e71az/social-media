39784fb26e565435cba5ea5ff0326b1cb6eaabce
s0kil <dtechtours@gmail.com>
2020-10-16 13:13:14 -0400

    Rubocop Fix

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  describe "Friendship model testing" do
-    it "Irreflexive Friendship test" do
+  describe 'Friendship model testing' do
+    it 'Irreflexive Friendship test' do
       user1, _user2 = create_users
       friendship = Friendship.new(user: user1, friend: user1)
 
       expect(friendship).to_not be_valid
     end
-    it "Succesful friendship" do
+    it 'Succesful friendship' do
       user1, user2 = create_users
       friendship = Friendship.new(user: user1, friend: user2)
 
