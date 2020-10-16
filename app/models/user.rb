61696e807ab1ebe77f25c9ee4a1b7cc6411ab45d
s0kil <dtechtours@gmail.com>
2020-10-16 16:20:35 -0400

    User pending_friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,5 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverted_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :confirmed_friendships, -> { where status: true }, class_name: 'Friendship'
+  has_many :pending_friendships, -> { where status: false }, class_name: 'Friendship'
 end
