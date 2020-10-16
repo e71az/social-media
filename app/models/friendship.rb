bc04afb0329358a1bb62057c31f2d77dc1b95493
s0kil <dtechtours@gmail.com>
2020-10-16 11:51:49 -0400

    Add Note Regarding Friendships Having Composite Key

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,5 @@
 class Friendship < ApplicationRecord
+  # [user_id, friend_id] merged to create a Composite Key
   self.primary_keys = :user_id, :friend_id
   belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
