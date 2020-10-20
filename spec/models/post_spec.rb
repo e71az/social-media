f01070d22aa404031b531c1ea4a641b25c8420eb
Daniel Sokil <dtechtours@gmail.com>
2020-10-20 11:23:43 -0400

    Rubocop Errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe 'Post model tests' do
     it 'Empty post content should not be valid' do
-      expect {create(:post, content: '')}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Content can't be blank")
+      expect { create(:post, content: '') }.to(
+        raise_error(ActiveRecord::RecordInvalid, "Validation failed: Content can't be blank")
+      )
     end
 
     it 'Post cannot be created by a blank user' do
-      expect {create(:post, user: nil)}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: User must exist")
+      expect { create(:post, user: nil) }.to(
+        raise_error(ActiveRecord::RecordInvalid, 'Validation failed: User must exist')
+      )
     end
   end
 end
