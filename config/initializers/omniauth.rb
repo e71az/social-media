1f3950b590e2ff91b510b211e9e1f0fc6b5ad7c1
Elias Castañeda <e71az@icloud.com>
2020-10-20 15:38:26 -0600

    Removed omniauth and added secret

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
-  scope: 'name, email, image', display: 'popup'
-
-end
